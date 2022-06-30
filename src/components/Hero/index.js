f7f8254c3aa45fe6276f6543eb531b8224c0f6f1
Alexandr <alexandr.lo@outlook.com>
2022-06-30 20:53:55 +0300

    Use context in hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,32 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import {
   Container,
   HStack,
   Heading,
   Show,
+  Skeleton,
+  SkeletonText,
   Text,
   VStack,
   useColorMode,
 } from "@chakra-ui/react";
 
+import { DataContext } from "components/DataProvider";
 import IllustrationDark from "assets/illustration/Illustration-dark.svg";
 import IllustrationLight from "assets/illustration/Illustration-dark.svg";
 import WavingHand from "./WavingHand";
-import portfolioData from "assets/json/portfolio.json";
 
 function Hero() {
+  const { portfolioData } = useContext(DataContext);
   const { colorMode } = useColorMode();
 
   return (
     <Container px={{ base: "1rem", md: "1.5rem" }} py="4rem">
       <HStack justify="space-between">
-        <VStack align="start" maxW="30rem" spacing="1rem">
+        <VStack align="start" w="100%" maxW="30rem" spacing="1rem">
           <Heading>
-            {portfolioData.hero.heading}
+            <Skeleton isLoaded={portfolioData} display="inline-block">
+              {portfolioData?.hero.heading ?? "Heading"}
+            </Skeleton>
             <WavingHand />
           </Heading>
-          <Text variant="lead">{portfolioData.hero.lead}</Text>
+          <SkeletonText
+            skeletonHeight={"1.5rem"}
+            noOfLines={3}
+            spacing="1rem"
+            w="100%"
+            isLoaded={portfolioData}
+          >
+            <Text variant="lead">{portfolioData?.hero.lead}</Text>
+          </SkeletonText>
         </VStack>
         <Show above="md">
           {colorMode === "light" && <IllustrationLight />}
